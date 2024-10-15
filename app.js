0be786944a8071a7e28796f3ee822709808b24d2
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2024-10-15 00:12:22 +0000

    Modelo Kubernets Concluido

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Olá, Kubernetes!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`API rodando na porta ${PORT}`);
+});
